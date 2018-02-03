283d5d02c01ef493ce499ef13804340c0e00460c
Araba Taylor <araba.taylor20@gmail.com>
2018-02-03 13:56:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,6 +35,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.names, COUNT()
-  FROM characters"
+  "SELECT characters.name, COUNT(books)
+  FROM characters
+  LEFT JOIN character_books ON characters.id = character_books.character_id
+  LEFT JOIN books ON books.id = character_id.book_id
+  GROUP BY characters.name;"
 end
