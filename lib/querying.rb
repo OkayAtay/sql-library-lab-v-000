57c6fbc2331ec66bdbd182344747fca58c75cfc6
Araba Taylor <araba.taylor20@gmail.com>
2018-02-03 14:15:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,8 @@ def select_series_title_with_most_human_characters
   FROM series
   LEFT JOIN characters ON characters.series_id = series.id
   WHERE characters.species = 'human'
-"
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
